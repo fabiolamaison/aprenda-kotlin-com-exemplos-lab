ecf533fcda0a23a32aa70a3cba1656a0a0d08773
Fabiolp <36681517+fabiolamaison@users.noreply.github.com>
2023-11-27 15:52:07 -0300

    Update desafio.kt
    Proposta inicial de solução do desafio proposto

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,65 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+// Versão inicial da resolução do desafio proposto //
+data class Usuario(var nome: String,var idade: Int,val email: String,val cpf: String)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel {BASICO, INTERMEDIARIO, AVANCADO}
 
-class Usuario
+enum class Tipo {CURSO, DESAFIO}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional (var nome: String,
+                                val nivel: Nivel,
+                                val tipo: Tipo,
+                                val duracao: Int = 60,
+                                var descricao: String ="",
+                                var icone: String = "") {
+    
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacoes (val nome: String,
+                      val nivel: Nivel,
+                      var conteudo: List<ConteudoEducacional>,
+                      var descricao: String ="",
+                      var duracao: Int = 0,
+                      var icone: String = "") {
+    
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    init {
+        attDuracao()
+    }
+    
+    fun attDuracao (){
+        for (i in conteudo) {
+            duracao += i.duracao
+        }
+    }
+    
+    fun matricular (usuario: Usuario) {
+        if(inscritos.contains(usuario)) {
+            println("Usuário já inscrito")
+        } else {
+            inscritos.add(usuario)
+            println("${usuario.nome} matriculado com sucesso")
+        }
+        
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+	val dAbstraindoKotlin: ConteudoEducacional = ConteudoEducacional("Abstraindo Formações da DIO Usando Orientação a Objetos com Kotlin", Nivel.BASICO, Tipo.DESAFIO, 120)
+    val dLogicaProgramacao: ConteudoEducacional = ConteudoEducacional("Desafios de Código: Aperfeiçoe Sua Lógica e Pensamento Computacional", Nivel.BASICO, Tipo.DESAFIO, 60)
+    val cPoderFuncoesKotlin: ConteudoEducacional = ConteudoEducacional("O Poder das Funções em Kotlin", Nivel.BASICO, Tipo.CURSO, 60)
+    
+    val backKotlinCont: MutableList<ConteudoEducacional> = mutableListOf(dAbstraindoKotlin, dLogicaProgramacao, cPoderFuncoesKotlin)
+    
+	val backKotlin: Formacoes = Formacoes("Desenvolvimento Back End com Kotlin", Nivel.BASICO, backKotlinCont)
+    
+    val usuario1: Usuario = Usuario("Fabio", 29, "fabio@endereco.com", "XXX.XXX.XXX-XX")
+    val usuario2: Usuario = Usuario("Aisha", 23, "aisha@endereco.com", "YYY.YYY.YYY-YY")
+    val usuario3: Usuario = Usuario("Mauricio", 18, "mauricio@endereco.com", "ZZZ.ZZZ.ZZZ-ZZ")
+    
+    backKotlin.matricular(usuario1)
+    backKotlin.matricular(usuario2)
+    backKotlin.matricular(usuario3)
+    
+    println(backKotlin)
 }
